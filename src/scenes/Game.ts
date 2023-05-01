599876d17ecc2a51294613649a0b1b9e3a0fef14
hawkstein <liamhawks+liamhawks@gmail.com>
2023-05-01 14:50:45 +0100

    add randomise control

--- src/scenes/Game.ts
+++ src/scenes/Game.ts
@@ -2,6 +2,8 @@ import Phaser from "phaser"
 import Scenes from "."
 import PlayerShip from "../game/PlayerShip"
 import WindZone from "../game/WindZone"
+import MultiKey from "../utils/MultiKey"
+import { Keys, getKey } from "../data"
 
 export type WindDirection = {
   rotate: number
@@ -18,6 +20,8 @@ export default class Game extends Phaser.Scene implements GameScene {
   public ship?: PlayerShip
   private windZone?: WindZone
   private directions: WindDirection[]
+  private castSpell?: MultiKey
+  private canSpellcast: boolean
 
   constructor() {
     super(Scenes.GAME)
@@ -31,17 +35,33 @@ export default class Game extends Phaser.Scene implements GameScene {
       { rotate: 45, speedX: 300, speedY: -300, angle: -45 },
       { rotate: 45, speedX: -300, speedY: 300, angle: 135 },
     ]
+    this.canSpellcast = true
   }
 
   create() {
     this.ship = new PlayerShip(this, 100, 100)
     this.windZone = new WindZone(this)
+    this.pickRandomWindRedirection()
+    this.castSpell = new MultiKey(this, getKey(Keys.ACTION))
+  }
+
+  pickRandomWindRedirection() {
     const direction = Phaser.Utils.Array.GetRandom(this.directions)
-    this.windZone.updateDirection(direction)
-    this.ship.setWindAngle(direction.angle)
+    this.windZone?.updateDirection(direction)
+    this.ship?.setWindAngle(direction.angle)
   }
 
   update() {
+    if (this.castSpell?.isDown() && this.canSpellcast) {
+      this.pickRandomWindRedirection()
+      this.canSpellcast = false
+      this.time.addEvent({
+        delay: 1000,
+        callback: () => {
+          this.canSpellcast = true
+        },
+      })
+    }
     this.ship?.update()
   }
 }
